69657a713972bda3f444586bffc86cdccc5f4b31
Greg Hobday <greg.hobday@csgi.com>
2021-03-27 13:27:09 -0500

    checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import CartItem from '../../components/cart-item/cart-item.component';
+
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -25,7 +28,9 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>Remove</span>
             </div>
         </div>
-        {cartItems.map(cartItem => cartItem.name)}
+        {cartItems.map(cartItem => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
